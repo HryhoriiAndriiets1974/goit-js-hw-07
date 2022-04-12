349b0d9ab89f9e6fa4705030fb37dc91ba04341c
Hryhorii Andriiets <ander_1_agg@ukr.net>
2022-04-12 21:01:48 +0300

    show_gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,19 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+galleryItems.forEach((elem) => {
+    gallery.insertAdjacentHTML(
+      "beforeend",
+      `<div class="gallery__item">
+    <a class="gallery__link" href="${elem.original}">
+      <img
+        class="gallery__image"
+        src="${elem.preview}"
+        data-source="${elem.preview}"
+        alt="${elem.description}"
+      />
+    </a></div>`
+    );
+  });
+  
