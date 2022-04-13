a31c353af0e3d60ec3ee8cf06703837a38bb2fcb
HryhoriiAndriiets1974 <ander_1_agg@ukr.net>
2022-04-13 12:32:22 +0300

    show_modal+script.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,19 +2,25 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+// галерея
 const gallery = document.querySelector(".gallery");
 galleryItems.forEach((elem) => {
-    gallery.insertAdjacentHTML(
-      "beforeend",
+    gallery.insertAdjacentHTML( "beforeend",
       `<div class="gallery__item">
-    <a class="gallery__link" href="${elem.original}">
-      <img
-        class="gallery__image"
-        src="${elem.preview}"
-        data-source="${elem.preview}"
-        alt="${elem.description}"
-      />
-    </a></div>`
-    );
+        <a class="gallery__link" href="${elem.original}">
+         <img
+          class="gallery__image"
+          src="${elem.preview}"
+          data-source="${elem.preview}"
+          alt="${elem.description}"
+         />
+        </a>
+      </div>`);
   });
-  
+// модалка
+  gallery.onclick = e => {
+    e.preventDefault();
+    if (e.target.nodeName !== 'IMG') {return;}
+    basicLightbox.create(`
+    <img src="${e.target.closest("a").href}">`).show();
+  }
