82d671785d4cc0c127651ec000cf2c04cab42822
HryhoriiAndriiets1974 <ander_1_agg@ukr.net>
2022-04-13 17:00:47 +0300

    hw-07-js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,17 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryItem = document.querySelector(".gallery");
+const murkup = galleryItems.map((e) =>
+	`<div class="gallery__item">
+		<a class="gallery__item" href="${e.original}">
+			<img class="gallery__image" src="${e.preview}" alt="${e.description}" />
+		</a>
+	</div>`)
+	.join("");
+galleryItem.insertAdjacentHTML("beforeend", murkup);
+
+let gallery = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
